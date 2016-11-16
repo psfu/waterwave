a38b6ed1b5e0951afaca16111972e7ddf7b7c0bb
psfu <psfu@outlook.com>
2016-11-16 16:24:54 +0800

    modify and implement nio method

---
+++ src/main/java/waterwave/net/nio/define/NioClientDataDealer.java
@@ -0,0 +1,34 @@
+/*
+ * Licensed to waterwave under one or more contributor
+ *  
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+
+package waterwave.net.nio.define;
+
+import java.nio.ByteBuffer;
+
+import waterwave.net.aio.AioClientChannel;
+
+public interface NioClientDataDealer {
+
+    
+	void clientBeforeRead(AioClientChannel channel);
+	void clientOnConnect(AioClientChannel channel);
+	void clientOnData(AioClientChannel channel, ByteBuffer buffer, int result);
+	void clientAfterWrite(AioClientChannel channel, ByteBuffer buffer, int bytes);
+	void clientOnError(AioClientChannel channel,Throwable exc, ByteBuffer attachment);
+    void clientOnClose(AioClientChannel channel);
+	boolean clientAcceptsMessages();
+}
